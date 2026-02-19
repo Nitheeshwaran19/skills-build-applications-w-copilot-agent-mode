51b66ab10d7126dfdae271eb1625c204a2adb51e
Nitheeshwaran19 <147901271+Nitheeshwaran19@users.noreply.github.com>
2026-02-19 02:33:12 +0000

    Clean up URLs configuration - remove unused CODESPACE_NAME logic

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,16 +1,9 @@
-
-import os
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
-codespace_name = os.environ.get('CODESPACE_NAME')
-if codespace_name:
-    base_url = f"https://{codespace_name}-8000.app.github.dev"
-else:
-    base_url = "http://localhost:8000"
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -21,6 +14,7 @@ def api_root(request, format=None):
         'leaderboard': reverse('leaderboard-list', request=request, format=format),
         'workouts': reverse('workout-list', request=request, format=format),
     })
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
